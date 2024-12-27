d5169e1cf3c305c3c68f1cd14d6c2082d0116395
Chima Obutte <obuttechima@rocketmail.com>
2024-12-27 04:42:13 +0000

    Update home view, home and base templates

--- home/models.py
+++ home/models.py
@@ -3,7 +3,10 @@ from django.db import models
 # Create your models here.
 class Home(models.Model):
     title = models.CharField(max_length=200)
-    body = models.TextField()
+    slogan = models.TextField()
+    menu = models.CharField(max_length=20)
+    dish = models.TextField()
+    description = models.TextField()
 
     def __str__(self):
         return self.title
