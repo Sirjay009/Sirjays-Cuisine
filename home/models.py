ed2ae09b9f7d89c92113903e70eaf605e19a202c
Chima Obutte <obuttechima@rocketmail.com>
2024-12-28 05:23:16 +0000

    Modify home app's models.py and template

--- home/models.py
+++ home/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Home(models.Model):
     title = models.CharField(max_length=200)
     slogan = models.TextField()
@@ -8,4 +10,3 @@ class Home(models.Model):
 
     def __str__(self):
         return self.title
-
