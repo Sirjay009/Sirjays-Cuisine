e79ffcf3bddecdd12315e1929c74e46bb9fd1fae
Chima Obutte <obuttechima@rocketmail.com>
2024-12-28 01:25:50 +0000

    Update home app's admin.py to SummernoteModelAdmin

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Home
+from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Home)
-class HomeAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slogan', 'content')
+class HomeAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('slogan', 'content',)
