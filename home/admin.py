bdb0b7b9c36549ebdb507772f085391a9220bd85
Chima Obutte <obuttechima@rocketmail.com>
2024-12-30 05:50:41 +0000

    Modify home app'sstyle sheet, home app's admin.py and template

--- home/admin.py
+++ home/admin.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from .models import Home, Menu
 from django_summernote.admin import SummernoteModelAdmin
 
+
 @admin.register(Home)
 class HomeAdmin(SummernoteModelAdmin):
 
     summernote_fields = ('slogan',)
+
 
 @admin.register(Menu)
 class MenuAdmin(admin.ModelAdmin):
