472c7cabf5917819fffed7cfb31a7a4946951699
Chima Obutte <obuttechima@rocketmail.com>
2025-01-07 00:09:14 +0000

    Update reservation/urls.py

--- reservation/urls.py
+++ reservation/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('reservation/', views.reservation_view, name='reservation'),
+    path('my-reservations/', views.my_reservations, name='my_reservations'),
+    path('reservation/<int:id>/edit/', views.edit_reservation, name='edit_reservation'),
+    path('reservation/<int:id>/delete/', views.delete_reservation, name='delete_reservation'),
 ]
