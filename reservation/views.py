241a56e2a1364cdb58d6def300fb20527e495d12
Chima Obutte <obuttechima@rocketmail.com>
2025-01-07 00:25:13 +0000

    Edit reservation/views.py file

--- reservation/views.py
+++ reservation/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 from django.contrib import messages
 from .forms import ReservationForm
 from .models import Reservation, Page
@@ -30,34 +29,3 @@ def reservation_view(request):
 
     return render(
         request, 'reservation/reservation_list.html', context)
-
-
-@login_required
-def my_reservations(request):
-    user_reservations = Reservation.objects.get(user=request.user)
-    return render(request, 'reservation/my_reservations.html', {'reservations': user_reservations})
-
-@login_required
-def edit_reservation(request, id):
-    reservation = get_object_or_404(Reservation, id=id, user=request.user)
-
-    if request.method == "POST":
-        form = ReservationForm(request.POST, instance=reservation)
-        if form.is_valid():
-            form.save()
-            return redirect('my_reservations')
-    else:
-        form = ReservationForm(instance=reservation)
-
-    return render(request, 'reservation/edit_reservation.html', {'form': form})
-
-@login_required
-def delete_reservation(request, id):
-    reservation = get_object_or_404(Reservation, id=id, user=request.user)
-
-    if request.method == "POST":
-        reservation.delete()
-        return redirect('my_reservations')
-
-    return render(request, 'reservation/confirm_delete.html', {'reservation': reservation})
-
