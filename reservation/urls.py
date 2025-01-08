26dbdcc5b21230f23d22f9b7545483b6a942e8a3
Chima Obutte <obuttechima@rocketmail.com>
2025-01-08 23:22:14 +0000

    Modify reservation view's url path

--- reservation/urls.py
+++ reservation/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:id>/', views.reservation_edit,
-         name="reservation_edit"),
     path('reservation/', views.reservation_list, name='reservation'),
+    path('<int:id>/edit/', views.reservation_edit,
+         name="reservation_edit"),
 ]
