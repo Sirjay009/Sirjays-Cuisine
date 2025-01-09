b1488201d97842dae7e45032a05cd463ae1a4b8d
Chima Obutte <obuttechima@rocketmail.com>
2025-01-09 01:38:17 +0000

    Add a url pattern for reservation_delete view

--- reservation/urls.py
+++ reservation/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('reservation/', views.reservation_list, name='reservation'),
     path('<int:id>/edit/', views.reservation_edit,
          name="reservation_edit"),
+    path('reservation/<int:id>/delete/', views.reservation_delete,
+         name="reservation_delete"),
 ]
