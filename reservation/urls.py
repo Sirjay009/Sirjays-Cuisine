c5a53a54a6a7e48228dc297aab960cfc76afc2e1
Chima Obutte <obuttechima@rocketmail.com>
2025-01-09 13:36:38 +0000

    Edit the URL path for the delete button

--- reservation/urls.py
+++ reservation/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('reservation/', views.reservation_list, name='reservation'),
+    path('', views.reservation_list, name='reservation'),
     path('<int:id>/edit/', views.reservation_edit,
          name="reservation_edit"),
-    path('reservation/<int:id>/delete/', views.reservation_delete,
+    path('delete_reservation/<int:id>', views.reservation_delete,
          name="reservation_delete"),
 ]
